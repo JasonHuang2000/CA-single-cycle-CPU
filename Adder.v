0f2e02ebb59118738cf0334959ad08d77faed576
Jason Huang <jason5528460@gmail.com>
2021-11-20 20:08:33 +0800

    finish Adder module

---
+++ Adder.v
@@ -0,0 +1,17 @@
+module Adder (
+    data1_in,
+    data2_in,
+    data_o
+);
+
+// Ports
+input   [31:0]  data1_i;
+input   [31:0]  data2_i;
+output  [31:0]  data_o;
+
+// Output result
+always @(data1_i or data2_i) begin
+    data_o <= data1_i + data2_i
+end
+
+endmodule
