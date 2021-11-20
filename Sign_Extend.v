4e622285df2e0fd697dacd043c453d7a52c67eb1
Jason Huang <jason5528460@gmail.com>
2021-11-20 19:28:39 +0800

    finish Sign Extend module

---
+++ Sign_Extend.v
@@ -0,0 +1,15 @@
+module Sign_Extend (
+    data_i,
+    data_o
+);
+
+// Ports
+input   [11:0]  data_i;
+output  [31:0]  data_o;
+
+// Output signal
+always @(data_i) begin
+    data_o <= { {20{data_i[11]}}, data_i }
+end
+
+endmodule
